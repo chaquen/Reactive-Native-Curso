449d32acf4dab93491c754f00a11ab1eb33de34b
chaquen <edgar.devmohan@gmail.com>
2020-01-08 22:08:13 -0500

    Se agrega ventaan de UserGuest, se agregan imagenes de logo y de userGuest

--- errors/readme.md
+++ errors/readme.md
@@ -1,5 +1,7 @@
 # Errores generados, 
 
+
+# 1
 ## console.error "React Native version mismatch":
 
 ### Descripcion.
@@ -14,4 +16,26 @@ Esto generara un proyecto, tenga en cuenta que el cliente expo de su celular deb
 ### Solución 
 Actualizar la aplicación Expo en el celular el cual cuenta con la version 36 de SDK
 
+# 2
 
+## console.error "Error: ENOSPC: System limit for number of file watchers reached":
+
+### Descripcion.
+Se muestra un mensaje "Error: ENOSPC: System limit for number of file watchers reached, watch ‘/home/ahmad/Desktop/new/jitsi-meet/node_modules/source-list-map’", a tratar de generar la aplicación 
+### Como generar error
+Ejecute el comando
+	
+	yarn dev
+
+Esto compilara el proyecto, pero generará el mensaje mensionado	
+
+### Solución 
+ ejecutar el comando
+
+	echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+### Solución tomada de
+
+	
+	https://community.jitsi.org/t/react-native-start-return-this-error/17919
+	https://stackoverflow.com/questions/22475849/node-js-what-is-enospc-error-and-how-to-solve 
--- miss-delicias/miss-delicias/app/screens/account/UserGuest.js
+++ miss-delicias/miss-delicias/app/screens/account/UserGuest.js
@@ -1,12 +1,62 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
+import { StyleSheet, View, ScrollView, 	Text, Image } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function UserGuest(){
 	return (
-		<View>
-			<Text>UserGuest Page</Text>
-		</View>
+		<ScrollView style={styles.viewBody} centerContent={true}>
+			<Image 
+				source = {require("../../../assets/img/user-guest.jpg")}
+				style={styles.image}
+				resizeMode='contain'
+			/>
+			<Text style={styles.title}>Consulta tu perfil en miss-delicias</Text>
+			<Text style={styles.description}>
+				¿Cómo descubririas los mejores restaurantes de tu ciudad?, 
+				encuentra los mejores restaurantes cerca de tu ciudad, comparte con tus amigos
+			</Text>
+			<View style={styles.viewBtn}>
+					<Button 
+						buttonStyle={styles.btnStyle}
+						containerStyle={styles.btnContainer}
+						title="Ver tu perfil"
+						onPress={()=>console.log("Ver perfíl presionado!")}
+					/>
+			</View>
+		</ScrollView>
 
 	);
 }
+
+const styles = StyleSheet.create({
+	viewBody: {
+		marginLeft: 30,
+		marginRight: 30
+	},
+	image:{
+		height: 300,
+		width: "100%",
+		marginBottom: 40
+	},
+	title:{
+		fontWeight: 'bold',
+		fontSize: 19,
+		marginBottom: 10,
+		textAlign: 'center'
+	},
+	description:{
+		textAlign: 'center',
+		marginBottom: 20 
+	},
+	viewBtn:{
+		flex: 1,
+		alignItems: 'center' 
+	},
+	btnStyle:{
+		backgroundColor: '#00a680'		
+	},
+	btnContainer:{
+		width: '70%'
+	}
+
+});
