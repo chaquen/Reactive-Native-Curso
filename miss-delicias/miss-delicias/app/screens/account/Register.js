c058838d50b09b99910e54f6764bd5b4f7b2e90a
chaquen <edgar.devmohan@gmail.com>
2020-01-25 13:55:43 -0500

    Se agrega nuevo paquete 'react-native-keyboard-aware-scroll-view' que permite hacer el scroll cuando se quiere escribir en un input, se agrego vista de screens/account/Register.js

--- miss-delicias/miss-delicias/app/screens/account/Register.js
+++ miss-delicias/miss-delicias/app/screens/account/Register.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default function Register(){
 
 	return (
-		<View>
-			<Text>Registro de usuarios</Text>
-		</View>
+		<KeyboardAwareScrollView>
+			<Image 
+					source = { require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+					style = { styles.logo }
+					resizeMode = 'contain'
+			/>
+			<View style={ styles.viewForm }>
+				<Text>Formulario de registro de usuarios</Text>
+			</View>	
+		</KeyboardAwareScrollView>
 
 	);
 }
+
+const styles = StyleSheet.create({
+	logo:{
+		width: '100%',
+		height: 150,
+		marginTop: 20
+
+	},
+	viewForm:{
+		marginTop: 40,
+		marginLeft: 40
+	}
+
+
+});
