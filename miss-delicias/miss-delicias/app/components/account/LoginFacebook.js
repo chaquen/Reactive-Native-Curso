24163398e5da946351a62791aa7a8dcf8df1dec8
chaquen <edgar.devmohan@gmail.com>
2020-03-04 18:19:05 -0500

    Se agrega logica para login con facebook, actauamente la version del expo es la 35, por o que genera error con la version de el paquete para facebook 8.0, se actaulizara la version de expo

--- miss-delicias/miss-delicias/app/components/account/LoginFacebook.js
+++ miss-delicias/miss-delicias/app/components/account/LoginFacebook.js
@@ -1,9 +1,35 @@
 import React from "react";
 import { SocialIcon } from "react-native-elements";
+import * as firebase from "firebase";
+import * as Facebook from 'expo-facebook';
+import { FacebookApi } from "../../utils/Social";
+import Loading from "../Loading";
+
 
 export default function LoginFacebook(){
-    const login =() => {
-        console.log("Inicio de sesión con facebook");
+    const login = async () => {
+        const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+                                            FacebookApi.application_id,
+                                            { permissions: FacebookApi.permissions } 
+                                      );
+        
+                                      if(type === "success"){
+                                        const credentials = firebase.auth.FacebookAuthProvider.credential(token);  
+                                        await firebase
+                                              .auth()
+                                              .signInWithCredential(credentials)
+                                              .then(()=>{
+                                                console.log("Bienvenidos desde facebook");    
+                                              })
+                                              .catch(() => {
+                                                console.log("Error accediendo a facebook");    
+                                              });  
+                                        
+                                      }else if(type === "cancel"){
+                                        console.log("Inicio de sesión cancelado");    
+                                      }else{
+                                        console.log("Error desconocido, por favor intenete mas tarde");    
+                                      }
     }
 
     return (
