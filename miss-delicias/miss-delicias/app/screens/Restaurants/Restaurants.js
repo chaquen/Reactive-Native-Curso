9c78ca6702071776d716deb3feb6b2a706de1932
chaquen <edgar.devmohan@gmail.com>
2020-03-18 16:50:53 -0500

    Se agrega componenete LIstRestaurant, para mostrar un listado de los restaurantes, se agragra consulta a la coleccion de los restaurantes y a sus respectivas imagenes

--- miss-delicias/miss-delicias/app/screens/Restaurants/Restaurants.js
+++ miss-delicias/miss-delicias/app/screens/Restaurants/Restaurants.js
@@ -1,19 +1,59 @@
 import React,{ useState, useEffect} from 'react';
 import {StyleSheet,View, Text} from 'react-native';
 import ActionButton from 'react-native-action-button';
-import * as firebase from 'firebase';
+import ListRestaurants from '../../components/Restaurants/ListRestaurants';
+import { firebaseApp } from '../../utils/FireBase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+const db = firebase.firestore(firebaseApp);
+
 
 export default function Restaurants(props){
  const {navigation}=props;
- const [ user, setUser] = useState(null)
+ const [ user, setUser ] = useState(null);
+ const [ restaurants,setRestaurants ] = useState([]);
+ const [ startRestaurants , setStartRestaurants ]=useState(null);
+ const [ isLoading,setIsLoading ] = useState(false);
+ const [ totalRestaurants,setTotalRestaurant]= useState(0);
+ const limitRestaurants = 8;
+  
  useEffect(()=>{
 	firebase.auth().onAuthStateChanged(userInfo => {
 		setUser(userInfo);
 	})
  },[]);
+ useEffect(()=> {
+	 db.collection("restaurants")
+	   .get()
+	   .then( snap => {
+		setTotalRestaurant(snap.size);
+	   });
+	   
+	 (async () => {
+		const resultRestaurant = [];
+		const restaurants = db
+						  .collection('restaurants')
+						  .orderBy('createAt','desc')
+						  .limit(limitRestaurants);
+		await restaurants.get().then(response => {
+			setStartRestaurants(response.docs[response.docs.length - 1]); 
+			
+			response.forEach(doc => {
+				
+				let restaurant = doc.data();
+				restaurant.id= doc.id;
+				resultRestaurant.push({restaurant});
+			});
+			setRestaurants(resultRestaurant);
+		})
+	 })()
+ },[]);
  return (
  	<View style={styles.viewBody}>
- 			<Text>Restaurantes</Text>
+ 			<ListRestaurants 
+				restaurants={restaurants}
+				isLoading={isLoading}
+			 />
 			{user && <AddRestaurantButton navigation={navigation}/>} 
  	</View>
  );
